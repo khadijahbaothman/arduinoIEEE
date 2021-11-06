ae084050712ac89ef5eef9c31946e63afdf3c4c6
khadijah baothman <67499169+khadijahbaothman@users.noreply.github.com>
2021-11-06 09:02:13 +0300

    Add files via upload

---
+++ servomotor.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+#include<Servo.h>
+Servo Myservo;
+int pos;
+void setup()
+{
+Myservo.attach(13);
+}
+
+void loop()
+{  
+for(pos=0;pos<=180;pos++){
+Myservo.write(pos);
+delay(15);
+}
+  delay(1000);
+  
+  for(pos=180;pos>=0;pos--){
+Myservo.write(pos);
+delay(15);
+}
+  delay(1000);
+  
+}
