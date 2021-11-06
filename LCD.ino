5402c320a4af2eb87c323d290946264eb886c6d2
khadijah baothman <67499169+khadijahbaothman@users.noreply.github.com>
2021-11-06 09:08:07 +0300

    Add files via upload

---
+++ LCD.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+ 
+ lcd.begin(16, 2);
+ 
+}
+
+void loop() {
+ 
+ 
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello world!!");
+  lcd.setCursor(0, 1);
+  lcd.print("IEEE");
+  lcd.setCursor(10, 1);
+delay(500);
+}
